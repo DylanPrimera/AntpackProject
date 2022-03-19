5dd667303e552b5a1ea85799f9a32bb1a0db6db9
DylanPrimera <dylanprimera98@gmail.com>
2022-03-19 17:34:33 -0500

    remove unused method and variables

--- src/app/components/smarts/dashboard/dashboard.component.ts
+++ src/app/components/smarts/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ApiService } from 'src/app/services/api.service';
 import { User } from 'src/app/util/interfaces/user';
 
@@ -7,21 +8,20 @@ import { User } from 'src/app/util/interfaces/user';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
   public users: User[];
-  public email: any;
+  public subscription = new Subscription();
   constructor(private apiServide: ApiService) { }
 
   ngOnInit(): void {
-    this.apiServide.getUsers().subscribe((data:any) => {
-        this.users = data;
-        this.apiServide.setSubUser(data);
-    })
+    this.subscription.add(this.apiServide.getUsers().subscribe((data:any) => {
+      this.users = data;
+  }));
 
   }
 
-  public getEmailToHash() {
-      this.apiServide.getSubUser().subscribe()
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,19 +7,10 @@ import { Observable, Subject } from 'rxjs';
 })
 export class ApiService {
   private URL: string = 'https://jsonplaceholder.typicode.com/users';
-  public user$ = new Subject<any>();
 
   constructor(private http: HttpClient) { }
 
   public getUsers(): Observable<any> {
     return this.http.get(this.URL);
-  }
-
-  public getSubUser() {
-    return this.user$.asObservable();
-  }
-
-  public setSubUser(user: any) {
-    return this.user$.next(user);
   }
 }
